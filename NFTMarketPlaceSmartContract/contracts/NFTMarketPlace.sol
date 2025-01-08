13a0440ee4c9a028cc609bbd5fdc6b7835ca12d5
vijayparmr27 <vijayparmar0027@gmail.com>
2025-01-08 23:37:50 +0530

    implement NFT marketplace contract with ERC721 support and listing functionality

--- NFTMarketPlaceSmartContract/contracts/NFTMarketPlace.sol
+++ NFTMarketPlaceSmartContract/contracts/NFTMarketPlace.sol
@@ -1,4 +1,93 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {Counters} from "./Counters.sol";
+import {console} from "hardhat/console.sol";
 
+contract NFTMarketPlace is ERC721Enumerable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    uint256 public listingPrice = 0.0025 ether;
+
+    address payable owner;
+
+    struct MarketItem {
+        uint tokenId;
+        address payable seller;
+        address payable owner;
+        uint price;
+        bool sold;
+    }
+
+    mapping(uint256 => MarketItem) private idMarketItem;
+
+    event idMarketItemCreated(
+        uint indexed tokenId,
+        address payable seller,
+        address payable owner,
+        uint price,
+        bool sold
+    );
+
+    constructor() ERC721("NFT Metaverse Token", "MyNFT") {
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function updateListingPrice(
+        uint256 _listingPrice
+    ) public payable onlyOwner {
+        listingPrice = _listingPrice;
+    }
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    function createToken(
+        string memory tokenURI,
+        uint price
+    ) public payable returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(msg.sender, newTokenId);
+        // _setTokenURI(newTokenId, tokenURI);
+
+        createMarkerItem(newTokenId, price);
+
+        return newTokenId;
+    }
+
+    function createMarkerItem(uint tokenId, uint price) public {
+        MarketItem memory newMarketItem = MarketItem(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+
+        idMarketItem[tokenId] = newMarketItem;
+
+        _transfer(msg.sender, address(this), tokenId);
+
+        emit idMarketItemCreated(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+    }
+}
--- NFTMarketPlaceSmartContract/ignition/modules/Lock.ts
+++ NFTMarketPlaceSmartContract/ignition/modules/Lock.ts
@@ -3,6 +3,8 @@
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
+
+
 const JAN_1ST_2030 = 1893456000;
 const ONE_GWEI: bigint = 1_000_000_000n;
 
