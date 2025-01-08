61d19233f6efc7834657387edbc750f856225c93
Vijay Parmar (NodeJS) <vparmar@elluminatiinc.com>
2025-01-08 16:36:07 +0530

    update README and contracts for Hardhat project setup and TypeScript integration

--- NFTMarketPlaceSmartContract/hardhat.config.ts
+++ NFTMarketPlaceSmartContract/hardhat.config.ts
@@ -1,9 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import '@typechain/hardhat'
-import '@nomicfoundation/hardhat-ethers'
-import '@nomicfoundation/hardhat-chai-matchers'
-import "tsconfig-paths/register";
+import "@typechain/hardhat";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
